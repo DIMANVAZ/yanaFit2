0fad77ee6ffdacd4d2830feab20c27de9e3909fa
dimanvaz <dimanvaz04@gmail.com>
2021-09-10 12:05:32 +0300

    feat: background

--- gallery.js
+++ gallery.js
@@ -2,10 +2,13 @@ let gallery = document.getElementById("gallery")
 //let cert1 = "<h1>ЗАГОЛОВОК</h1>" для примера, что работает
 let amountOfCerts = 35;
 
+let folder=["compressedCertif","WebP_encoded"]
+let extension=['png','webp']
+
 let emptyCertsStructure = ``;
 
 for (let i = 1; i <= amountOfCerts; i++) {
-    emptyCertsStructure += `<img class="small-image" src="./media/certif/cert-${i}.png" alt='certificate'/> <hr>`
+    emptyCertsStructure += `<img src="./media/${folder[1]}/cert-${i}.${extension[1]}" class="small-image" alt='certificate'/> <hr>`
 }
 
 // gallery.innerHTML = cert2
